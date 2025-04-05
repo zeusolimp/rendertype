5508e2edaa12b12d7ae88b900986b1a21e253e1e
Ender Guevara Narea <enderguevara@outlook.com>
2025-04-05 16:44:01 +0100

    Modificacion de Titulo con mi nombre en TopBar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,7 +1,7 @@
 export const TopBar = () => {
   return (
     <header className="bg-slate-900 text-white p-5 text-center">
-        <h1>Modos de Renderizado en Next JS</h1>
+        <h1>Modos de Renderizado en Next JS <span className="font-bold text-blue-500">Ender Guevara Narea</span></h1>
 
     </header>
   );
